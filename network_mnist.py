1989e33214eadeea3435f7bed9763dfe23b8c32a
AidarSource <34319725+AidarSource@users.noreply.github.com>
2021-04-05 23:11:47 +0600

    add 'RELU' activation and 'Adam' optimizer

--- network_mnist.py
+++ network_mnist.py
@@ -1,11 +1,10 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import SGD
+from keras.layers import Dense, BatchNormalization
+from keras.optimizers import Adam
+from keras.initializers import glorot_normal
 from keras.utils import to_categorical
 from keras.datasets import mnist
-from keras.layers import BatchNormalization
-import losses
 
 (X_train_raw, Y_train_raw), (X_test_raw, Y_test_raw) = mnist.load_data()
 X_train = X_train_raw.reshape(X_train_raw.shape[0], -1) / 255
@@ -15,20 +14,18 @@ Y_train = to_categorical(Y_train_raw)
 Y_validation, Y_test = np.split(to_categorical(Y_test_raw), 2)
 
 model = Sequential()
-model.add(Dense(1200, activation='sigmoid'))
+model.add(Dense(1200, activation='relu'))
 model.add(BatchNormalization())
-model.add(Dense(500, activation='sigmoid'))
+model.add(Dense(500, activation='relu'))
 model.add(BatchNormalization())
-model.add(Dense(200, activation='sigmoid'))
+model.add(Dense(200, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dense(10, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=SGD(lr=0.1),
+              optimizer=Adam(),
               metrics=['accuracy'])
 
 history = model.fit(X_train, Y_train,
                     validation_data=(X_validation, Y_validation),
                     epochs=10, batch_size=32)
-
-losses.plot(history)
